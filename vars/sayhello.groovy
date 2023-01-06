ee971810501b3372d6ef4ba3c46f277f017dc6cf
pankajkandpal12 <pankaj.kandpal44@gmail.com>
2023-01-06 04:40:52 -0500

    first commit

--- vars/sayhello.groovy
+++ vars/sayhello.groovy
@@ -9,35 +9,5 @@ def call(body) {
     }
      
      print "build commands are ${pipelineParams.buildCommands[0]}"
-     def buildCommands = pipelineParams.buildCommands
-     buildJava(buildCommands)
-     def buildJava(buildCommands){
-
-     
-     stage('Application Build'){
-
-     agent {
-        docker {
-            image 'maven:3-alpine'
-            args '-v /root/.m2:/root/.m2'
-        }
-     }
-    try {
-
-       if (buildCommands!=null){
-        echo "Running a build command override:"
-        sh "$buildCommands"
-    }
-    catch(Exception e) {
-      
-    }
-    finally {
-      
-    }
     
-
-     }
-     }
-
-}
 }
