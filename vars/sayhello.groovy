a9da971dda83ded2ea86ccc8a3fd7bcd9ea7f111
pankajkandpal12 <pankaj.kandpal44@gmail.com>
2023-01-06 04:48:37 -0500

    first commit

--- vars/sayhello.groovy
+++ vars/sayhello.groovy
@@ -16,12 +16,7 @@ def call(body) {
  def buildJava(buildCommands){
  stage('Application Build'){
 
-     agent {
-        docker {
-            image 'maven:3-alpine'
-            args '-v /root/.m2:/root/.m2'
-        }
-     }
+     
     try {
 
        if (buildCommands!=null){
